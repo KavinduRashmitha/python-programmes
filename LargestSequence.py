4586fe761f5cb7ce8cb3231038a5be94d1a6c77c
KavinduRashmitha <kavinduwelivitigoda@gmail.com>
2026-06-01 17:23:38 +0530

    Update LargestSequence.py

--- LargestSequence.py
+++ LargestSequence.py
@@ -1,8 +1,7 @@
 n=input("Enter Values: ").split(",")
 y=n.pop(0)
-y=list(y)
+y=[int(y)]
 n=list(map(int,n))
-y=list(map(int,y))
 x=0
 new=[]
 for i in n:
@@ -25,7 +24,7 @@ for i in new:
         lenth=len(i)
         large.clear()
         large.append(i)
-    elif len(i)==lenth:
+    elif len(i)==lenth and i not in large:
         large.append(i)
 
 print("***Largest Sequence/s***")
