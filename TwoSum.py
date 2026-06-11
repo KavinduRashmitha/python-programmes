134cf021803896d71a3788742f4cf98d02eb86af
KavinduRashmitha <kavinduwelivitigoda@gmail.com>
2026-06-11 22:19:37 +0530

    version-2

--- TwoSum.py
+++ TwoSum.py
@@ -1,15 +1,10 @@
 x=input("Enter Values: ").split(",")
-x=list(set(map(int,x)))
+x=set(map(int,x))
 n=int(input("Enter Target Number: "))
-new=[]
+new=set()
 
 for i in x:
     num=n-i
-    if num==i:
-        continue
-    else:
-        if num in x:
-            if i not in new:
-                new.append(i)
-                new.append(num)
-                print(i,",",num)
+    if num in new:
+        print(i,",",num)
+    new.add(i)
