3a4b06f7bec644dc34f04462ad102dc01a1d788b
KavinduRashmitha <kavinduwelivitigoda@gmail.com>
2026-06-14 16:24:55 +0530

    version-3

--- TwoSumIndex.py
+++ TwoSumIndex.py
@@ -1,11 +1,11 @@
 x=input("Enter Values: ").split(",")
 x=list(map(int,x))
+y=set(x)
 n=int(input("Enter Target Number: "))
-new=set()
+new=dict()
 
 for i in x:
     num=n-i
-    if num in x:
-        if (x.index(num)) in new:
-            print(x.index(i),",",x.index(num))
-        new.add(x.index(i))
+    if num in y and x.index(num) in new.values():
+        print(new[num],x.index(i))
+    new[i]=x.index(i)
